ba1fa1fdea968ba354a4e450c79b80171f078632
Nils Diekmann <nils.diekmann@sinc.de>
2026-06-24 16:20:44 +0200

    - bring code example nearer to api sdk

--- 00-hello-claude.ts
+++ 00-hello-claude.ts
@@ -1,7 +1,20 @@
-import { query } from "@anthropic-ai/claude-agent-sdk";
+import { query, SDKUserMessage } from "@anthropic-ai/claude-agent-sdk";
+
+
+async function* messages(): AsyncGenerator<SDKUserMessage> {
+  yield {
+    type: "user",
+    parent_tool_use_id: null,
+    session_id: "",
+    message: {
+      role: "user",
+      content: "Hello, Claude",
+    },
+  };
+}
 
 const response = query({
-  prompt: "Hello, Claude",
+  prompt:  messages(),
   options: {
     model: "claude-haiku-4-5",
     maxThinkingTokens: 1024 // original maxToken
